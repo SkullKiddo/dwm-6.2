960ca20c54cbfbdb84ca332c296a88a13c861905
Gabriel <gabrielpuigdemunt@gmail.com>
2021-01-17 20:16:30 +0000

    fecha en status bar

--- statusBar/set_xsetroot.sh
+++ statusBar/set_xsetroot.sh
@@ -7,6 +7,8 @@ export SEP2="]"
 # Import the modules
 . "`dirname "${0}"`/bar-functions/dwm_battery.sh"
 . "`dirname "${0}"`/bar-functions/dwm_alsa.sh"
+. "`dirname "${0}"`/bar-functions/dwm_date.sh"
+
 
 # . "$DIR/bar-functions/dwm_countdown.sh"
 # . "$DIR/bar-functions/dwm_alarm.sh"
@@ -22,16 +24,15 @@ export SEP2="]"
 # . "$DIR/bar-functions/dwm_networkmanager.sh"
 # . "$DIR/bar-functions/dwm_keyboard.sh"
 # . "$DIR/bar-functions/dwm_ccurse.sh"
-# . "$DIR/bar-functions/dwm_date.sh"
 # . "$DIR/bar-functions/dwm_connman.sh"
 # . "$DIR/bar-functions/dwm_loadavg.sh"
 # . "$DIR/bar-functions/dwm_currency.sh"
 
 # Append results of each func one by one to a string
 dispstr=""
-dispstr="$dispstr$(dwm_battery)"
-
-dispstr="$dispstr$(dwm_alsa)"
+#dispstr="$dispstr$(dwm_battery)"
+#dispstr="$dispstr$(dwm_alsa)"
+dispstr="$dispstr$(dwm_date)"
 
 # dispstr="$dispstr$(dwm_connman)"
 # dispstr="$dispstr$(dwm_pulse)"
@@ -49,7 +50,6 @@ dispstr="$dispstr$(dwm_alsa)"
 # dispstr="$dispstr$(dwm_networkmanager)"
 # dispstr="$dispstr$(dwm_keyboard)"
 # dispstr="$dispstr$(dwm_ccurse)"
-# dispstr="$dispstr$(dwm_date)"
 # dispstr="$dispstr$(dwm_loadavg)"
 # dispstr="$dispstr$(dwm_currency)"
 
